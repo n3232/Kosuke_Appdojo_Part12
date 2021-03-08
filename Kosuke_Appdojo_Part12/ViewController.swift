3aaace93bd20c59c6b1e04869f593e827dafcbd8
Kosuke Nagao <kosukengo@gmail.com>
2021-03-08 20:48:00 +0900

    完成

--- Kosuke_Appdojo_Part12/ViewController.swift
+++ Kosuke_Appdojo_Part12/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var taxExcludedAmountTextField: UITextField!
+    @IBOutlet private weak var salesTaxRateTextField: UITextField!
+    @IBOutlet private weak var taxIncludedAmountLabel: UILabel!
+
+    @IBAction private func pressCalculateButton(_ sender: Any) {
+        let taxExcludedAmount = Float(taxExcludedAmountTextField.text!) ?? 0
+        UserDefaults.standard.set(salesTaxRateTextField.text!, forKey: "salesTaxRate")
+        let salesTaxRate = Float(salesTaxRateTextField.text!) ?? 0
+        let taxIncludedAmount = Int(taxExcludedAmount + taxExcludedAmount * salesTaxRate / 100)
+        taxIncludedAmountLabel.text = String(taxIncludedAmount)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let salesTaxRateText = UserDefaults.standard.string(forKey: "salesTaxRate")
+        salesTaxRateTextField.text = salesTaxRateText
     }
-
-
 }
-
